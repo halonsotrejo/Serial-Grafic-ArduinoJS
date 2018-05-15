7e4b030255596d740124cc2be68018fcb32a68a3
Hector Trejo <htrejo@hydra.com>
2018-05-15 11:27:38 -0500

    commit con cambios

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+
 const socketIO = require('socket.io');
 const http = require('http');
 
@@ -7,35 +8,35 @@ const server = http.createServer(app);
 
 const io = socketIO.listen(server);
 
-io.onconnection('connection', function(socket){
+io.on('connection', function(socket){
     Console.log('nuevo socket conectado');
 });
 
-app.get('/', (req, res, next)=>{
-    res.sendFile(__dirname + 'index.html');
+app.get('/', (req, res, next) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
 const SerialPort = require('serialport');
-const ReadLine = SerialPort.parsers.Readline;
-const parse = new Readline();
+const Readline = SerialPort.parsers.Readline;
+const parser = new Readline();
 
-const mySerial = new SerialPort('COM3',{
+const mySerial = new SerialPort('COM8',{
     baudRate: 9600
 });
 
-mySerial.open('open',function(){
+mySerial.open('open', function(){
     console.log('Puerto abierto');
 });
 
 mySerial.open('data', function(data){
-    console.log(data.toString);
+    //console.log(data.toString);
     io.emit('arduino:data',{
         value: data.toString()
     });
 });
 
-mySerial.open('err', function(err){
-    console.log(err.message);
+mySerial.open('error', function(error){
+    console.log(error.message);
 });
 
 server.listen(3000, function(){
